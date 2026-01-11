353da077fb87a7642b314e1f7c901d8f222c372f
rajveer2450 <rajveerpatel.2450@gmail.com>
2026-01-11 12:41:34 +0530

    new ch

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
-const Pool= require('pg').Pool;
+const Pool = require('pg').Pool;
 
-const pool= new Pool({
-    user: 'postgres',
-    password: 'admin',
-    host: 'localhost',
-    port: 5432,
-    database: 'perntodo'
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
-//comment
-module.exports= pool;
+
+module.exports = pool;
