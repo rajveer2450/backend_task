fe8b0f133abc56a4fd63b4d85c012edace62adee
rajveer2450 <rajveerpatel.2450@gmail.com>
2026-01-14 13:04:19 +0530

    new chang

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const port = process.env.PORT || 5000;
 const pool = require('./db');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'ia ad dja dd abdadsafdfbhjfbafhabfj';
+const corsOptions = {
+  origin: 'https://cheery-bonbon-ce1403.netlify.app/', // Your Netlify URL
+  credentials: true
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.listen(port, () => { console.log(`Server running on port ${port}`); });
 
